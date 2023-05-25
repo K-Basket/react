451c1fae226704b323102085e069cc30cfab69e7
K-Basket <koshykkv@gmail.com>
2023-05-25 16:55:21 +0300

    add useSearchParams

--- src/components/Router/componentsRouter/Products.jsx
+++ src/components/Router/componentsRouter/Products.jsx
@@ -1,14 +1,14 @@
 import ProductsList from './ProductsList/ProductsList';
 
 const products = [
-  { id: '1', name: 'Product 1', color: 'red' },
-  { id: '2', name: 'Product 2', color: 'black' },
-  { id: '3', name: 'Product 3', color: 'blue' },
-  { id: '4', name: 'Product 4', color: 'white' },
-  { id: '5', name: 'Product 5', color: 'black' },
-  { id: '6', name: 'Product 6', color: 'green' },
-  { id: '7', name: 'Product 7', color: 'green' },
-  { id: '8', name: 'Product 8', color: 'green' },
+  { id: '1', name: 'Banana', color: 'red' },
+  { id: '2', name: 'Apricot', color: 'black' },
+  { id: '3', name: 'Cherry', color: 'blue' },
+  { id: '4', name: 'Pineapple', color: 'white' },
+  { id: '5', name: 'Pear', color: 'black' },
+  { id: '6', name: 'Mango', color: 'green' },
+  { id: '7', name: 'Strawberry', color: 'green' },
+  { id: '8', name: 'Watermelon', color: 'green' },
 ];
 
 const Products = () => {
--- src/components/Router/componentsRouter/ProductsList/ProductsList.jsx
+++ src/components/Router/componentsRouter/ProductsList/ProductsList.jsx
@@ -1,4 +1,5 @@
 // import { Link } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { Container, LinkStyled } from './ProductsListStyled';
 
 const ProductsList = ({ products }) => {
@@ -6,11 +7,29 @@ const ProductsList = ({ products }) => {
   //   // HTTP запрос реализуется здесь
   // }, [])
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = searchParams.get('params') ?? ''; // читает строку запроса
+  const filteredProducts = products.filter(el => el.name.includes(params));
+
+  const updateQueryString = evt => {
+    if (evt.target.value === '') setSearchParams({});
+    setSearchParams({ params: evt.target.value });
+  };
+
   return (
     <>
+      <input
+        type="text"
+        value={params}
+        onChange={updateQueryString} // перезаписывает строку запроса
+      />
+      <button onClick={() => setSearchParams({ params: 'basket' })}>
+        change Search
+      </button>
+
       <h3>Products List:</h3>
       <Container>
-        {products.map(product => (
+        {filteredProducts.map(product => (
           <LinkStyled key={product.id} to={`${product.id}`}>
             <p>{product.name}</p>
           </LinkStyled>
