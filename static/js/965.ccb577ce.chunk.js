733a37c856db80f4573c9937f43c09e889278d9d
K-Basket <114851751+K-Basket@users.noreply.github.com>
2023-05-25 21:42:28 +0000

    Deploying to gh-pages from @ K-Basket/react@a83350ab3c4dcdde5503d8015ca1d47cb06218d2 🚀

---
+++ static/js/965.ccb577ce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact=self.webpackChunkreact||[]).push([[965],{965:function(e,c,n){n.r(c);var r=n(184);c.default=function(){return(0,r.jsx)("h3",{children:"Home page"})}}}]);
+//# sourceMappingURL=965.ccb577ce.chunk.js.map
