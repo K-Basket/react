03baeb13b7356f531e85dbb969adb02bfbe93638
K-Basket <koshykkv@gmail.com>
2023-06-11 17:51:17 +0300

    add Persist (localStorage)

--- src/components/Redux/store/reducer.js
+++ src/components/Redux/store/reducer.js
@@ -1,6 +1,7 @@
 // ========================= Ниже код Redux ToolKit  ====================== //
 
 // Разбиваем логику reducer на несколько функций
+import { combineReducers } from 'redux'; // обязательно нужен для persist
 
 import { counterReducer } from './counter/counterSlice';
 import { todoReducer } from './todo/todoReducer';
@@ -8,10 +9,10 @@ import { todoReducer } from './todo/todoReducer';
 
 // для экспорта объединяем reducer с помошью комбайна
 // объединяет сколько угодно reducer-ов в один объект
-export const reducer = {
+export const reducer = combineReducers({
   counter: counterReducer,
   todo: todoReducer,
-};
+});
 
 // ========================= Ниже код чистый Redux ====================== //
 
--- src/components/Redux/store/store.js
+++ src/components/Redux/store/store.js
@@ -1,11 +1,41 @@
 // ========================= Ниже код Redux ToolKit  ====================== //
 
 import { configureStore } from '@reduxjs/toolkit';
-// import { reducer as rootReducer } from './reducer';
-// или то же самое
 import { reducer } from './reducer';
-// configureStore обязательно принимает щбъект
-export const store = configureStore({ reducer });
+
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'todo',
+  storage,
+  blacklist: ['todo'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+// configureStore обязательно принимает объект
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// весь state будет записываться в LocalStorrage
+export const persistor = persistStore(store);
 
 // ========================= Ниже код чистый Redux ====================== //
 
